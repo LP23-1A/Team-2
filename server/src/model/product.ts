1ffa82236a71fe5461a307cff0612dd268db971b
Otgonbyr <otgonbayar0503@gmail.om>
2024-03-10 22:04:21 +0800

    product frontend

--- server/src/model/product.ts
+++ server/src/model/product.ts
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const productMongoose = new mongoose.Schema({
     productName : String,
-    categoryID : String,
+    categoryID: {
+        type: mongoose.Schema.Types.ObjectId,
+      },
     price : Number,
     qty : Number,
     thumbnails : String,
