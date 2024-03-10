e650f87e25e976d95c0f6ad7c1db96d91c5311ed
Otgonbyr <otgonbayar0503@gmail.om>
2024-03-11 00:53:54 +0800

    product frontend hiiv

--- client/src/app/admin/product/page.tsx
+++ client/src/app/admin/product/page.tsx
@@ -1,9 +1,23 @@
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 import ProductMain from "@/components/Product";
 
 const product = () => {
   return(
     <main>
-      <ProductMain/>
+      <Navbar />
+      <div className="flex h-screen">
+        <Menu />
+        <div className=" w-screen bg-[#ECEDF0]">
+          <div className="flex gap-[30px] p-[20px]">
+            <button className="p-[10px]">Бүтээгдэхүүн</button>
+            <button className="p-[10px]">Ангилал</button>
+          </div>
+          <div className="ml-[20px]">
+            <ProductMain/>
+          </div>
+        </div>
+      </div>
     </main>
   )
 };
