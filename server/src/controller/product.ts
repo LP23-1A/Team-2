aeaaa9bbac3571c6658b2a3b7d6225826ee21843
Otgonbyr <otgonbayar0503@gmail.om>
2024-03-11 23:17:35 +0800

    s3 aws

--- server/src/controller/product.ts
+++ server/src/controller/product.ts
@@ -1,8 +1,35 @@
 import { Request, Response } from "express";
+import multer from "multer";
 import { productSchema } from "../model/product";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
+
+upload.array('avatar')
+
+const bucketName = process.env.BUCKET_NAME
+const bucketRegion = process.env.BUCKET_REGION
+const accessKey = process.env.ACCESS_KEY
+const secretAccessKey = process.env.SECRET_ACCESS_KEY
+
+const s3 = new S3Client({
+    credentials: {
+      accessKeyId: accessKey,
+      secretAccessKey: secretAccessKey
+    },
+    region : bucketRegion,
+  })
 
 export const createProduct = async (req : Request, res : Response) => {
     try {
+        const params = {
+            Bucket : bucketName
+        }
+        const command = new PutObjectCommand(params)
         const {productName : productName, description : description, price : price, qty : qty} = req.body
         const result = await productSchema.create({productName : productName, description : description, price : price, qty : qty, })
         console.log(result);
