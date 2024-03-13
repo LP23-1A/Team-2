76a329f1cfefc3d769f682be71cb6c6d218d99ff
Нацагдорж Мөнхбаяр <23LP2745@G303-CC0129.local>
2024-03-13 12:31:02 +0800

    add useRef and userSign

--- client/src/app/admin/signAuth/page.tsx
+++ client/src/app/admin/signAuth/page.tsx
@@ -1,16 +1,18 @@
 "use client";
 import axios from "axios";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { Arrow } from "../../../components/images/arrow";
-import { useRouter } from "next/navigation";
 import { useAuth0 } from "@auth0/auth0-react";
+import Google from "@/components/images/google";
 
+type Auth0ProviderProps = {
+  domain: string;
+  clientId: string;
+  props: any;
+};
 const SignPage = () => {
   const { user } = useAuth0();
-  console.log(user);
   const [formData, setFormData] = useState({
-    name: "",
-    email: "",
     shopName: "",
     city: "",
     district: "",
@@ -20,8 +22,6 @@ const SignPage = () => {
     user: user,
   });
   const {
-    name,
-    email,
     shopName,
     city,
     district,
@@ -29,10 +29,8 @@ const SignPage = () => {
     shoppingExprient,
     shoppingType,
   } = formData;
-  console.log(user);
-  
+
   const [activeStep, setActiveStep] = useState(0);
-  const { loginWithRedirect } = useAuth0();
   const nextStep = () => {
     setActiveStep((prevStep) =>
       prevStep === steps.length - 1 ? prevStep : prevStep + 1
@@ -50,59 +48,36 @@ const SignPage = () => {
       console.log(err, "axios error");
     }
   };
-
-  const handleOnChange = (field: string, value: string | number) => {
-    formDataRef.current = { ...formDataRef.current, [field]: value };
-  };
-
+  console.log(user);
   const steps = [
-    <div className="flex items-center justify-center h-[100vh]">
-      <div className="border-solid border-2 border-[#ECEDF0] rounded-[8px] p-[40px] ">
-        <div className="w-[404px] box-border mx-auto flex flex-col gap-8  items-center ">
-          <h1 className=" text-[32px] font-bold">Бүртгүүлэх</h1>
-          <div className="flex gap-4 flex-col">
-            <div className="w-[404px] box-border mx-auto flex flex-col gap-3 ">
-              <div className="flex flex-col gap-1 ">
-                <p className="font-normal text-[16px]">Таны имэйл </p>
-                <input
-                  className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
-                  type="email"
-                  name="email"
-                  placeholder="Имэйл"
-                  onChange={(e) => handleOnChange("email", e.target.value)}
-                />
-                <p className="font-semibold text-[16px] text-red-600 ">
-                  {error.email}
-                </p>
-              </div>
-              <div className="flex flex-col gap-1 ">
-                <p className="font-normal text-[16px]">Таны нэр </p>
-                <input
-                  className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
-                  type="text"
-                  name="name"
-                  placeholder="Нэр"
-                  onChange={(e) => handleOnChange("name", e.target.value)}
-                />
-              </div>
-              <div className="flex flex-col gap-1 ">
-                <p className="font-normal text-[16px]">Таны Утасны дугаар</p>
-                <input
-                  className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
-                  type="text"
-                  name="Утасны дугаар"
-                  placeholder="Утасны дугаар"
-                  onChange={(e) =>
-                    handleOnChange("phoneNumber", e.target.value)
-                  }
-                />
-                <p className="font-semibold text-[16px] text-red-600 ">
-                  {error.phoneNumber}
-                </p>
-              </div>
-            </div>
+    <div className="flex items-center h-[100vh]">
+      <div className="w-[404px] box-border mx-auto justify-center flex flex-col gap-8">
+        <h1 className=" text-[32px] font-bold ">Дэлгүүрийн мэдээлэл</h1>
+        <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-1">
+            <p className="font-semibold text-[16px]">
+              Танай дэлгүүрийн нэр юу вэ?
+            </p>
+            <input
+              className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
+              type="text"
+              name="shopName"
+              value={shopName}
+              placeholder="Дэлгүүрийн нэр"
+              onChange={(e) =>
+                setFormData({ ...formData, shopName: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex justify-between">
             <button
-              className="bg-black text-white rounded-[8px] h-[56px]  "
+              className="w-[48px] flex justify-center items-center h-[48px] rounded-[50px] bg-[#1C20240A]"
+              onClick={prevStep}
+            >
+              <Arrow />
+            </button>
+            <button
+              className="bg-black text-white rounded-[8px] h-[56px] w-[127px]  "
               onClick={nextStep}
             >
               Дараах
@@ -112,15 +87,12 @@ const SignPage = () => {
       </div>
     </div>,
     <div className="flex items-center h-[100vh]">
-      <div className="w-[404px] box-border mx-auto justify-center flex flex-col gap-8">
-        <h1 className=" text-[32px] font-bold ">Хаяг</h1>
-        <div className="flex flex-col gap-8">
-          <div className="flex flex-col gap-1">
-            <div>
-              <p className="font-normal text-[16px]">Таны Хаяг</p>
-              <p className="text-[14px]">
-                (Жишээ нь Баянзүрх дүүрэг 13 хороо 56-р байр 24 тоот)
-              </p>
+      <div className="w-[404px] box-border mx-auto justify-center flex flex-col">
+        <div className="w-[404px] box-border mx-auto flex flex-col gap-8">
+          <h1 className=" text-[32px] font-bold ">Бүс нутгийн мэдээлэл</h1>
+          <div className="flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <p className="font-semibold text-[16px]">Хот/Аймаг</p>
               <input
                 className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
                 type="text"
@@ -132,8 +104,8 @@ const SignPage = () => {
                 }
               />
             </div>
-            <div>
-              <p className="font-normal text-[16px]">Zipcode</p>
+            <div className="flex flex-col gap-1">
+              <p className="font-semibold text-[16px]">Сум/Дүүрэг</p>
               <input
                 className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
                 type="text"
@@ -178,10 +150,15 @@ const SignPage = () => {
     </div>,
     <div className="flex items-center h-[100vh]">
       <div className="w-[404px] box-border mx-auto flex flex-col gap-8">
-        <h1 className="text-[32px] font-bold">Нууц үг</h1>
+        <h1 className="text-[32px] font-bold">Жоохон танилцья</h1>
+        <span className="text-[16px] font-normal ">
+          Энэ мэдээллийг дэлгүүрийн тохиргоонд туслах зорилгоор ашиглана.
+        </span>
         <div className="flex flex-col gap-3">
           <div className="flex flex-col gap-1">
-            <p className="font-semibold text-[16px]">Нууц үг </p>
+            <p className="font-semibold text-[16px]">
+              Та борлуулалт хийж байсан туршлагатай юу?
+            </p>
             <input
               className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
               type="text"
@@ -195,7 +172,7 @@ const SignPage = () => {
           </div>
           <div className="flex flex-col gap-1">
             <p className="font-semibold text-[16px]">
-              Нууц үгээ давтан оруулна уу
+              Та ямар төрлийн бүтээгдэхүүн борлуулах вэ?
             </p>
             <input
               className="border-2 rounded-[8px] bg-[#F7F7F8] w-[404px] h-[56px] box-border border-gray-300 p-2"
