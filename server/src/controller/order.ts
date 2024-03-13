324e049fc025199d4f30771d588bf5fd3eb5f1f0
NyamjargalMTMN <metamancode@gmail.com>
2024-03-14 02:55:21 +0800

    income

--- server/src/controller/order.ts
+++ server/src/controller/order.ts
@@ -1,12 +1,10 @@
 import { Response, Request } from 'express';
 import {OrderModel} from "../model/order"
+import {AdminModel} from "../model/adminSign"
 
  const newOrder = async (req: Request, res: Response) => {
   try {
-    await OrderModel.create(req.body);
-    console.log(req.body);
-    console.log("eee");
-    
+    await OrderModel.create(req.body); 
     return res.status(201).send({ success: true });
   } catch (error) {
     return res.status(400).send({ error });
@@ -15,21 +13,28 @@ import {OrderModel} from "../model/order"
 
 const getAllOrders = async (req: Request, res: Response) => {
     try {
-      const orderData = await OrderModel.find().populate('users')
-      // return res.status(201).send({ success: true });
-      res.status(201).json(orderData);
+      const orderData = await OrderModel.find();
+      return res.status(201).send({ success: true, orderData });
+      // res.status(201).json(orderData);
+    } catch (error) {
+      return res.status(400).send({ error });
+    }
+  };
+
+  const getIncome = async (req: Request, res: Response) => {
+    try {
+      const orderData = await OrderModel.find().populate("userid");
+      return res.status(201).send({ success: true, incomeData: orderData });
+
+      
+      // res.status(201).json(orderData);
     } catch (error) {
       return res.status(400).send({ error });
     }
   };
 
   const updateOrderById = async (req: Request, res: Response) => {
-     const orderId = req.body;
-     console.log(req.params);
-     
-     console.log(orderId);
-      // console.log(req);
-      
+     const orderId = req.body;  
     try {
     
       const updateData = await OrderModel.findByIdAndUpdate(orderId, req.body, {new:true});  
@@ -40,5 +45,5 @@ const getAllOrders = async (req: Request, res: Response) => {
     }
   };
 
-  export {newOrder, getAllOrders, updateOrderById}
+  export {newOrder, getAllOrders, updateOrderById, getIncome}
   
