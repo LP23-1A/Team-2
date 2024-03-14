e203aba501adb739a95ef188bc3d476c05f5e77a
NyamjargalMTMN <metamancode@gmail.com>
2024-03-14 12:00:10 +0800

    order data baazaas medeelel haruulah

--- client/src/app/admin/order/page.tsx
+++ client/src/app/admin/order/page.tsx
@@ -5,21 +5,18 @@ import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 import Searchbar from "@/components/svg/Searchbar";
 import Download from "@/components/images/Download.png";
-const API = "http://localhost:8000/order";
+const API = "http://localhost:8000/order/getorder";
 
 export default function Order() {
   const [data, setdata] = useState([]);
 
-  const Incomehandler = async () => {
+  const Orderhandler = async () => {
     let res = await axios.get(API);  
-   setdata(res.data.incomeData);
-   console.log(res);
-   
- 
+   setdata(res.data.orderData);
   };
 
   useEffect(() => {
-    Incomehandler();
+    Orderhandler();
   }, []);
 
   return (
@@ -70,6 +67,11 @@ export default function Order() {
                 const year = date.getFullYear();
                 const month = String(date.getMonth()+1).padStart(2,'0');
                 const day = String(date.getDate()).padStart(2,'0');
+                const time = new Date(dateString);
+                const hours = String(time.getUTCHours()).padStart(2, '0');
+                const minutes = String(time.getUTCMinutes()).padStart(2, '0');
+                const formattedTime = `${hours}:${minutes}`;
+                
                 const formatteddate = `${year}-${month}-${day}`;
 
                 const amount = el.amountPaid;
@@ -77,13 +79,16 @@ export default function Order() {
 
                   return (
                     <div key={idp} className="flex border-t-2">              
-                      <p className=" pl-6 py-[14px] w-[269px] text-[14px] flex items-center">{el._id}</p>
-                      <div className="pl-6 py-[14px] w-[268px] text-[14px]">
-                      {el.userid.email} <br /> {el.phoneNumber}
-                      </div>
-                   
+                      <p className=" pl-6 py-[16px] w-[291px] text-[12px] flex items-center">{el._id}</p>
+                      <div className="pl-6 py-[16px] w-[209px] text-[12px]">
+                      {el.userid.name} <br />   {el.userid.email}
+                      </div>                  
+                      <p  className="pl-6 py-[14px] w-[168px] text-[14px] flex items-center">{formatteddate}</p>
+                      <p  className="pl-6 py-[14px] w-[129px] text-[14px] flex items-center">{formattedTime}</p>
                       <p  className=" pl-6 py-[14px] w-[137px] text-[14px] flex items-center">{formattedamount}</p>
-                      <p  className="pl-6 py-[14px] w-[150px] text-[14px] flex items-center">{formatteddate}</p>
+                      <p  className=" pl-6 py-[14px] w-[214px] text-[14px] flex items-center">Хүргэгдсэн</p>
+                      <button className=" pl-6 py-[14px] w-[122px] text-[14px] flex items-center justify-center"> > </button>
+               
                     </div>        
                              
                   );
