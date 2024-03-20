64525feaea672eedea114e90a34f70479f4e5176
Nakii <115760470+Nakii0603@users.noreply.github.com>
2024-03-20 20:49:52 +0800

    add checkrole and loading page

--- client/src/app/admin/checkRole/page.tsx
+++ client/src/app/admin/checkRole/page.tsx
@@ -1,32 +1,52 @@
-'use client'
-import React, { useEffect } from "react";
-import axios from "axios";
-import { useAuth0 } from "@auth0/auth0-react";
-import { useRouter } from "next/navigation";
+'use client';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useRouter } from 'next/navigation';
+import { CircularProgress } from '@mui/material';
 
 export default function CheckRole() {
   const { user } = useAuth0();
-  const userInfo = user?.email;  
+  const userInfo = user?.email;
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const handleCheckRole = async () => {
       try {
-        const api = "http://localhost:8000/admin/admincheckrole";
+        const api = 'http://localhost:8000/admin/admincheckrole';
         const response = await axios.post(api, {
-          role: "admin", 
+          role: 'admin',
         });
         if (response.data.email === userInfo) {
-          router.push("dashboard")
+          setTimeout(() => {
+            router.push('dashboard');
+          }, 1500);
         } else {
-          console.log("User does not have admin role");
+          console.log('User does not have admin role');
         }
       } catch (err) {
-        console.log(err, "axios error");
+        console.log(err, 'axios error');
+      } finally {
+        setLoading(false);
       }
     };
     handleCheckRole();
   }, [userInfo]);
 
-  return <div>loading...</div>;
+  if (loading) {
+    return (
+      <div>
+        <div>Loading...</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] '>
+      <CircularProgress color='secondary' />
+      <CircularProgress color='success' />
+      <CircularProgress color='inherit' />
+    </div>
+  );
 }
