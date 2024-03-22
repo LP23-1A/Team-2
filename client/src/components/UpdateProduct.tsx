b480a0fec3ade5267206d775786d412dfa017dd8
Отгонбаяр Лхасүрэн <23LP4826@G303-CC0128.local>
2024-03-22 09:46:11 +0800

    update product

--- client/src/components/UpdateProduct.tsx
+++ client/src/components/UpdateProduct.tsx
@@ -73,15 +73,73 @@ export default function updateProduct() {
                         <input value={caption} onChange={handleCaptionChange} type="text" placeholder='Caption'></input>
                         <button type="submit">Submit</button>
                     </form> */}
-          <div className="bg-[white] p-[25px] text-[18px] font-[600] rounded-md text-[black]">
-            <p>Бүтээгдэхүүний зураг</p>
-            <div className="mt-[10px] flex gap-[10px]">
-              <div className="w-[125px] h-[125px] rounded-md border-dashed border-2"></div>
+                    <div className="bg-[white] p-[25px] text-[18px] font-[600] rounded-md text-[black]">
+                        <p>Бүтээгдэхүүний зураг</p>
+                        <div className="mt-[10px] flex gap-[10px]">
+                            <div className="w-[125px] h-[125px] rounded-md border-dashed border-2"></div>
+                        </div>
+                    </div>
+                    <div className=" bg-[white] p-[25px] flex gap-[20px] rounded-md text-[black]">
+                        <div>
+                            <p>Үндсэн үнэ</p>
+                            <input className="bg-[#f2f2f2] py-[6px] px-[10px] rounded-md border-2 border-[#e0dfdf] mt-[5px]" type="text" placeholder="Үндсэн үнэ" 
+                            value={price}
+                            onChange={(event) => setPrice(event.target.value)}
+                            />
+                        </div>
+                        <div>
+                            <p>Үлдэгдэл тоо ширхэг</p>
+                            <input className="bg-[#f2f2f2] py-[6px] px-[10px] rounded-md border-2 border-[#e0dfdf] mt-[5px]" type="text" placeholder="Үлдэгдэл тоо ширхэг" 
+                            value={qty} 
+                            onChange={(event) => setQty(event.target.value)}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="w-[600px] flex flex-col gap-[25px] text-[black]">
+                    <div className="p-[25px] bg-[white] flex flex-col gap-[15px] rounded-md">
+                        <div>
+                            <p>Ерөнхий ангилал</p>
+                            <select onChange={(event) => setCategoryID(event.target.value)} className="w-full bg-[#f2f2f2] py-[8px] px-[12px] rounded-md border-2 border-[#e0dfdf] mt-[5px]" id="Ерөнхий ангилал">
+                                <option value="Эрэгтэй">Эрэгтэй</option>
+                                <option value="Эмэгтэй">Эмэгтэй</option>
+                                <option value="Хүүхэд">Хүүхэд</option>
+                            </select>
+                        </div>
+                        <div>
+                            <p>Дэд ангилал</p>
+                            <select onChange={(event) => setCategory(event.target.value)} className="w-full bg-[#f2f2f2] py-[8px] px-[12px] rounded-md border-2 border-[#e0dfdf] mt-[5px]" id="Ерөнхий ангилал">
+                                <option value="Tsamts">Tsamts</option>
+                                <option value="Umd">Umd</option>
+                                <option value="Gutal">Gutal</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="p-[25px] bg-[white] flex flex-col gap-[15px] rounded-md">
+                        <p className="font-[600] text-[18px]">Төрөл</p>
+                        <div className="flex flex-col gap-[10px]">
+                            <div className="flex gap-[10px]">
+                                <p>Өнгө</p>
+                                <button className="w-[30px] h-[30px] bg-[#f2f2f2] flex text-align justify-center rounded-full">+</button>
+                            </div>
+                            <div className="flex gap-[10px]">
+                                <p>Хэмжээ</p>
+                                <button className="w-[30px] h-[30px] bg-[#f2f2f2] flex text-align justify-center rounded-full">+</button>
+                            </div>
+                        </div>
+                        <button className="w-[140px] border-2 border-[#e0dfdf] rounded-md bg-[white] py-[5px] px-[8px]">Төрөл нэмэх</button>
+                    </div>
+                    <div className="p-[25px] bg-[white] flex flex-col rounded-md h-[192px]">
+                        <p className="font-[600] text-[16px]">Таг</p>
+                        <input className="w-full bg-[#f2f2f2] py-[6px] px-[10px] rounded-md border-2 border-[#e0dfdf] mt-[5px]" type="text" placeholder="Таг нэмэх..." />
+                        <p className="text-[#b8b8b8]">Санал болгох: Гутал , Цүнх , Эмэгтэй </p>
+                    </div>
+                </div>
             </div>
             <div className="pl-[940px]">
                 <div className="flex gap-[15px] ">
                     <button className="py-[10px] px-[20px] border-2 border-[#e0dfdf] bg-[white] rounded-md">Ноорог</button>
-                    <button onClick={() => {handleUpdate(); router.push("/admin/product");}} 
+                    <button onClick={() => {handleUpdate()}} 
                     className="py-[10px] px-[20px] bg-[black] text-[white] rounded-md">Нийтлэх</button>
                 </div>
             </div>
