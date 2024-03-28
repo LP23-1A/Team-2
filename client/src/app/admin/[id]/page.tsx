ef292628cf7be8a6b2e898eacdcf5bd65a17127b
Нямжаргал Энхбаяр <23LP7263@G303-CC0148.local>
2024-03-28 11:23:45 +0800

    order details

--- client/src/app/admin/[id]/page.tsx
+++ client/src/app/admin/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import axios from "axios";
@@ -6,13 +7,15 @@ import useSWR from "swr";
 import Menu from "@/components/Menu";
 import Navbar from "@/components/Navbar";
 
-export default function Orderdetails() { 
+export default function Orderdetails() {
   const pathName = usePathname();
   // const [data, setData] = useState({id:"",status:"",email:""});
   const [data, setData] = useState([]);
   const Orderhandler = async () => {
     const res = await axios.get(`http://localhost:8000/order/${pathName}`);
     setData(res.data.orderData);
+    console.log(res.data.orderData);
+
   };
   // Orderhandler();
   useEffect(() => {
@@ -35,17 +38,37 @@ export default function Orderdetails() {
                   <p className="text-[16px] text-[#3F4145]">
                     Захиалгын ID дугаар:
                   </p>
-                  <p className="text-[16px] text-[#121316]">{data._id}</p>
+                  <p className="text-[16px] text-[#121316] font-bold">{data._id}</p>
                 </div>
                 <div><p>{data.status}</p></div>
               </div>
-              <div>
+              <div className="flex flex-col gap-8">
+                <div>
                 <p className="text-[16px] text-[#3F4145]">
                   Захиалагч: Хувь хүн
                 </p>
-                <div>    <p className="text-[16px] text-[#3F4145]"></p>
+                <div className=" flex gap-3">    
+                  <p className="text-[16px] text-[#3F4145] font-bold">{data?.userId?.name}</p>
+                  <p className="text-[16px] text-[#3F4145]">{data?.userId?.email}</p>
+                  <p className="text-[16px] text-[#3F4145]">{data?.phoneNumber}</p>
                 </div>
+                </div>
+            
+                <div className="flex"> 
+                      <img src={data.productId?.images} alt=""  className="w-[180px]"/>
+                      <div className=" flex flex-col gap-3">
+                      <h1>{data?.productId?.productName}</h1>
+                        <div>
+                 
+                        <p>{data?.productId?.createdAt}</p>
 
+                        <div className="flex gap-1"><p>Бүтээгдэхүүний ID:</p><p>{data?.productId?.productId}</p></div>
+                        </div>
+              
+                        <p className=" font-bold">{data?.amountPaid}</p>
+                      </div>
+                </div>
+                
               </div>
             </div>
             <div>
