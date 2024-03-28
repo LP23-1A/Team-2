1835b3e74d19687c59e2c5bc5c673f995fafdcc6
Нямжаргал Энхбаяр <23LP7263@G303-CC0148.local>
2024-03-28 13:08:22 +0800

    product cart

--- server/src/controller/order.ts
+++ server/src/controller/order.ts
@@ -1,63 +1,63 @@
 import { Response, Request } from 'express';
-import {orderModel} from "../model/order"
+import { orderModel } from "../model/order"
 
 const newOrder = async (req: Request, res: Response) => {
   try {
-    await orderModel.create(req.body); 
+    await orderModel.create(req.body);
     return res.status(201).send({ success: true });
   } catch (error) {
-    return res.status(400).send({success: false, error });
+    return res.status(400).send({ success: false, error });
   }
 };
 
 const getAllOrders = async (req: Request, res: Response) => {
-    try {
-      const orderData = await orderModel.find();
-      return res.status(201).send({ success: true, orderData });
-    } catch (error) {
-      return res.status(400).send({success: false, error });
-    }
-};
-
-  const getIncome = async (req: Request, res: Response) => {
-    try {
-      const orderData = await orderModel.find().populate("userId");
-      return res.status(201).send({ success: true, incomeData: orderData });
-    } catch (error) {
-      return res.status(400).send({success: false, error });
-    }
-  };
-
-  const updateOrderById = async (req: Request, res: Response) => {
-    const {id} = req.params;  
-    try {
-      const updatedData = await orderModel.findByIdAndUpdate(id, req.body, {new:true});  
-      return res.status(201).send({ success: true, updatedData });
-    } catch (error) {
-      return res.status(400).send({success: false, error });
-    }
-  };
-
-  const deleteOrderById = async (req: Request, res: Response) => {
-   const {id} = req.params;  
-   try {
-     const deletedData = await orderModel.findByIdAndDelete(id);  
-     return res.status(201).send({ success: true, deletedData });
-   } catch (error) {
-    return res.status(400).send({success: false, error });
-   }
- };
-
- const getOrderById = async (req: Request, res: Response) => {
-  const {id} = req.params;  
   try {
-    const orderData = await orderModel.findById(id).populate("userId").populate("productId");  
+    const orderData = await orderModel.find();
     return res.status(201).send({ success: true, orderData });
   } catch (error) {
-   return res.status(400).send({success: false, error });
+    return res.status(400).send({ success: false, error });
+  }
+};
+
+const getIncome = async (req: Request, res: Response) => {
+  try {
+    const orderData = await orderModel.find().populate("userId");
+    return res.status(201).send({ success: true, incomeData: orderData });
+  } catch (error) {
+    return res.status(400).send({ success: false, error });
+  }
+};
+
+const updateOrderById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const updatedData = await orderModel.findByIdAndUpdate(id, req.body, { new: true });
+    return res.status(201).send({ success: true, updatedData });
+  } catch (error) {
+    return res.status(400).send({ success: false, error });
+  }
+};
+
+const deleteOrderById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const deletedData = await orderModel.findByIdAndDelete(id);
+    return res.status(201).send({ success: true, deletedData });
+  } catch (error) {
+    return res.status(400).send({ success: false, error });
+  }
+};
+
+const getOrderById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const orderData = await orderModel.findById(id).populate("userId").populate("productId");
+    return res.status(201).send({ success: true, orderData });
+  } catch (error) {
+    return res.status(400).send({ success: false, error });
   }
 };
 
 
-  export {newOrder, getAllOrders, updateOrderById, getIncome,  deleteOrderById, getOrderById}
-  
+export { newOrder, getAllOrders, updateOrderById, getIncome, deleteOrderById, getOrderById }
+
